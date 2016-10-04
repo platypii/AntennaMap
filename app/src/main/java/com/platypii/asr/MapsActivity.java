1bd5626e8ef1d85f016eb5d819a2d2ece560b04b
Kenny Daniel <platypii@gmail.com>
2016-10-04 00:20:48 -0700

    Update to latest google maps api

--- app/src/main/java/com/platypii/asr/MapsActivity.java
+++ app/src/main/java/com/platypii/asr/MapsActivity.java
@@ -22,7 +22,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
@@ -34,7 +33,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class MapsActivity extends FragmentActivity implements GoogleMap.OnCameraChangeListener, GoogleMap.OnInfoWindowClickListener, OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnCameraMoveListener, GoogleMap.OnInfoWindowClickListener, OnMapReadyCallback, GoogleMap.OnCameraIdleListener {
+    private static final String TAG = "Map";
 
     private static MapsActivity instance = null;
 
@@ -90,33 +90,38 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
             try {
                 map.setMyLocationEnabled(true);
             } catch(SecurityException e) {
-                Log.e("Map", "Error enabling location", e);
+                Log.e(TAG, "Error enabling location", e);
             }
         } else {
             // request the missing permissions
             final String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
             ActivityCompat.requestPermissions(this, permissions, MY_PERMISSIONS_REQUEST_LOCATION);
         }
-        if (firstLoad) {
+        final LatLng center = map.getCameraPosition().target;
+        if (firstLoad || (Math.abs(center.latitude) < 0.1 && Math.abs(center.longitude) < 0.1)) {
             final Location myLocation = getMyLocation();
             if (myLocation != null
                     && 8 < myLocation.getLatitude() && myLocation.getLatitude() < 76
                     && -170 < myLocation.getLongitude() && myLocation.getLongitude() < -60) {
                 final LatLng home = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-                Log.w("Map", "Centering map on " + home);
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(home, 8));
+                Log.w(TAG, "Centering map on " + home);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 8));
             } else {
                 // final LatLng seattle = new LatLng(47.61, -122.34);
                 final LatLng usa = new LatLng(41.2, -120.5);
-                Log.w("Map", "Centering map on default view " + usa);
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(usa, 5));
+                Log.w(TAG, "Centering map on default view " + usa);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(usa, 5));
             }
             firstLoad = false;
         }
 
         // Drag listener
-        map.setOnCameraChangeListener(this);
+        map.setOnCameraMoveListener(this);
+        map.setOnCameraIdleListener(this);
         map.setOnInfoWindowClickListener(this);
+
+        // Get initial update
+        mUpdateMap();
 
         Log.w("Map", "Map ready");
     }
@@ -125,13 +130,13 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if(requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
             if(grantResults.length == 1 &&
-                    permissions[0] != null && permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    Manifest.permission.ACCESS_FINE_LOCATION.equals(permissions[0]) &&
                     grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 if(map != null) {
                     try {
                         map.setMyLocationEnabled(true);
                     } catch(SecurityException e) {
-                        Log.e("Map", "Error enabling location", e);
+                        Log.e(TAG, "Error enabling location", e);
                     }
                 }
             }
@@ -147,7 +152,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
         try {
             myLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         } catch(SecurityException e) {
-            Log.w("Map", "Permission denied for GPS location");
+            Log.w(TAG, "Permission denied for GPS location");
         }
 
         // Location wasn't found, check the next most accurate place for the current location
@@ -161,10 +166,10 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
                 try {
                     myLocation = lm.getLastKnownLocation(provider);
                 } catch (SecurityException e) {
-                    Log.w("Map", "Permission denied for GPS location");
+                    Log.w(TAG, "Permission denied for GPS location");
                 }
             } else {
-                Log.w("Map", "No location provider found");
+                Log.w(TAG, "No location provider found");
             }
         }
 
@@ -181,26 +186,41 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
     // Drag listener
     private static final long QUERY_WAIT_TIME = 500; // millis
     private boolean running = false;
-    private long lastDrag = 0;
+    private long lastDrag = System.currentTimeMillis();
 
     /**
      * Class to update map only once every QUERY_WAIT_TIME milliseconds
      */
     private final Runnable runnable = new Runnable() {
         @Override public void run() {
-        if(System.currentTimeMillis() < lastDrag + QUERY_WAIT_TIME) {
-            mUpdateMap();
-            // Schedule again
-            handler.postDelayed(runnable, QUERY_WAIT_TIME);
-        } else {
-            // Let it die
-            running = false;
-        }
+            if(System.currentTimeMillis() < lastDrag + QUERY_WAIT_TIME) {
+                mUpdateMap();
+                // Schedule again
+                handler.postDelayed(runnable, QUERY_WAIT_TIME);
+            } else {
+                // Let it die
+                running = false;
+            }
         }
     };
 
     @Override
-    public void onCameraChange(CameraPosition position) {
+    public void onCameraMove() {
+//        Log.d(TAG, "onCameraMove()");
+        mapMoved();
+    }
+
+    @Override
+    public void onCameraIdle() {
+//        Log.d(TAG, "onCameraIdle()");
+        handler.removeCallbacks(runnable);
+        mUpdateMap();
+    }
+
+    /**
+     * Call this when the map has moved, or should be updated
+     */
+    private void mapMoved() {
         lastDrag = System.currentTimeMillis();
         if(!running) {
             running = true;
@@ -212,15 +232,16 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
     private boolean querying = false;
     private void mUpdateMap() {
         if(map == null) {
-            Log.e("Map", "Update called, but map not ready");
+            Log.e(TAG, "Update called, but map not ready");
         } else if(querying) {
-            Log.w("Map", "Update called, but still querying");
+            Log.w(TAG, "Update called, but still querying");
+            mapMoved(); // Ensure that we retry
         } else {
             final LatLngBounds bounds = map.getProjection().getVisibleRegion().latLngBounds;
             if (bounds.southwest.latitude == 0 && bounds.southwest.longitude == 0 && bounds.northeast.latitude == 0 && bounds.northeast.longitude == 0) {
-                Log.e("Map", "Update called on empty view bounds");
+                Log.e(TAG, "Update called on empty view bounds");
             } else {
-                // Log.i("Map", "Updating map...");
+                Log.d(TAG, "Updating antennas");
                 querying = true;
 
                 // Query in the background
@@ -287,13 +308,13 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
 
     @Override
     public void onInfoWindowClick(Marker marker) {
-        Log.w("Map", "Clicked marker info");
+        Log.w(TAG, "Clicked marker info");
         // Find which marker
         for(Map.Entry<ASRRecord,Marker> entry : markers.entrySet()) {
             if(entry.getValue().equals(marker)) {
                 final ASRRecord record = entry.getKey();
                 // Open url
-                Log.w("Map", "Opening url for tower " + record.id);
+                Log.w(TAG, "Opening url for tower " + record.id);
                 final Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(record.url()));
                 startActivity(browserIntent);
             }
